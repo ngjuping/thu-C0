e4553a6aa0775a3971206b150dd668e24e38522b
pb0316 <756488714@qq.com>
2020-11-18 20:24:52 +0800

    调整了代码结构，将venus和courts相关代码放进venus文件夹

--- Qinghuiyue/Qinghuiyue/models/models.py
+++ Qinghuiyue/Qinghuiyue/models/models.py
@@ -3,25 +3,6 @@ import bson
 import bcrypt
 
 
-
-class Venue(DynamicDocument):
-    name=StringField()
-    intro=StringField()
-    courts=ListField()
-    image=StringField()
-    venue_id=IntField()
-    notices=ListField()
-
-class Court(DynamicDocument):
-    name=StringField()
-    venue=ObjectIdField()
-    status_now=StringField()
-    enum_id=IntField()#运动类型
-    rent_queue=ListField()
-    draw_list=ListField()
-    rent_for_long=ListField()
-    Status=ListField()
-
 class Feedback(DynamicDocument):
     user_id=IntField()
     time=DateTimeField()
