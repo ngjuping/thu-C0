c6a8a3b48332f57e94a2ffa5d1dd5334bd5c6906
ngjuping <ngjuping2018@163.com>
2020-12-20 04:25:11 +0800

    style: 美化header

--- vue-frontend/src/components/Qheader.vue
+++ vue-frontend/src/components/Qheader.vue
@@ -1,22 +1,26 @@
 <template>
-    <nav class="navbar bg-white shadow">
-        <div @click="$router.push({name:'Mainpage'})" id="logo" class="d-flex align-items-center">
-            <img src="@/assets/logo.png" width="100" class="d-inline-block align-top" alt="" id="logoimg">
-            <span id="logotext" class="rounded bg-dark text-light p-2">回到主页</span>
-        </div>
-        <div>
-            <div class="btn-group mr-2" v-if="this.$store.state.logged_in && !this.$store.state.privilege">
-                <div class="btn btn-primary" @click="$router.push({name:'Manage'})">我的场地</div>
-                <div class="btn btn-success" @click="$router.push({name:'AllShares'})">拼场广场</div>
-                <div class="btn btn-warning" @click="$router.push({name:'AllFeedbacks'})">反馈天地</div>
-                <div class="btn btn-info" @click="$router.push({name:'AllCourses'})">培训武馆</div>
+    <nav class="navbar navbar-expand-lg bg-white shadow">
+                <div @click="$router.push({name:'Mainpage'})" id="logo" class="d-flex align-items-center w-50 my-2">
+                    <img src="@/assets/logo.png" width="100" class="d-inline-block align-top" alt="" id="logoimg">
+                    <span id="logotext" class="rounded bg-dark text-light p-2">回到主页</span>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
+                    <font-awesome-icon icon="bars" class="w-50 h-50" style="max-width:50px;" />
+                </button>
+                <div class="btn-group mr-2 collapse navbar-collapse" 
+                v-if="this.$store.state.logged_in && !this.$store.state.privilege" 
+                id="navbarTogglerDemo02">
+                    <div class="btn btn-primary" @click="$router.push({name:'Manage'})">我的场地</div>
+                    <div class="btn btn-success" @click="$router.push({name:'AllShares'})">拼场广场</div>
+                    <div class="btn btn-warning" @click="$router.push({name:'AllFeedbacks'})">反馈天地</div>
+                    <div class="btn btn-info" @click="$router.push({name:'AllCourses'})">培训武馆</div>
+                <button class="btn btn-primary" v-if="!this.$store.state.logged_in"  @click="gotoLogin">登录</button>
+                <button class="btn btn-danger" v-else @click="logout">登出</button>
+                
+                <!-- <small v-if="this.$store.state.logged_in">已上线：{{ now() }}</small> -->
             </div>
-            
-            <button class="btn btn-primary" v-if="!this.$store.state.logged_in"  @click="gotoLogin">登录</button>
-            <button class="btn btn-danger" v-else @click="logout">登出</button>
-            <br/>
-            <!-- <small v-if="this.$store.state.logged_in">已上线：{{ now() }}</small> -->
-        </div>
+        
+        
         
     </nav>
 </template>
@@ -98,7 +102,6 @@ export default {
     nav:hover #logoimg{
         opacity:0.5;
     }
-
     #title{
         font-size:30px;
     }
@@ -106,6 +109,19 @@ export default {
     @media (max-width:760px) {
         #title{
             font-size:25px;
+        }
+
+        #logo{
+            width:100%;
+        }
+
+        #logotext{
+            opacity:1;
+            font-size:20px;
+        }
+
+        #logoimg{
+            display:none !important;
         }
     }
     @media (max-width:500px) {
