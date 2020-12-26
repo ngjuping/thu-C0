93ce6c2e55fac3e1405d5c0ed8e3a04a016efbda
ngjuping <ngjuping2018@163.com>
2020-12-26 14:01:35 +0800

    Fix: 补充main前端遗失文件

---
+++ vue-frontend/src/views/Signup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SignupForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SignupForm from '@/components/SignupForm.vue'
+
+export default {
+  components: {
+    SignupForm
+  }
+}
+</script>
