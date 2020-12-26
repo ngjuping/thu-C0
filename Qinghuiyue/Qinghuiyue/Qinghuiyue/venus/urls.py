948a489045c2fc266d4378909866d18ba639f52d
ngjuping <ngjuping2018@163.com>
2020-12-26 22:36:55 +0800

    拷贝复制最新(12月26日)后端到main分支上

--- Qinghuiyue/Qinghuiyue/Qinghuiyue/venus/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-import Qinghuiyue.venues.views as views
-
-urlpatterns = [
-    path('main/venues', views.get_venues_info),
-    path('main/venues/list', views.get_venues_list),
-    path('booking', views.get_courts_info)
-]
